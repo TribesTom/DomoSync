2b52b7e534b0065ebac43740d7ef17372797b1d9
TribesTom <tribes.tom@gmail.com>
2014-06-30 22:49:42 +0200

    Add first step to ethernet connection support

--- src/DomoDaemon.c
+++ src/DomoDaemon.c
@@ -486,6 +486,93 @@ void *deviceMonitoring(void* arg)
     udev_device_unref(dev);
 }
 
+
+void *deviceEthernetMonitoring(void* arg)
+{
+    int thID=(int)syscall(SYS_gettid);
+	int socket_desc , new_socket , c , *new_sock;
+    struct sockaddr_in server , client;
+    char *message;
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        syslog(LOG_INFO,"Thread % : Could not create socket",thID);
+    }
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        syslog(LOG_INFO,"Thread % : bind failed",thID);
+        return 1;
+    }
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    syslog(LOG_INFO,"Thread % : Waiting for incoming connections...",thID);
+    c = sizeof(struct sockaddr_in);
+    while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        syslog(LOG_INFO,"Thread % : Connection accepted");
+         
+        //Reply to the client
+         
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = new_socket;
+         
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        {
+            syslog(LOG_INFO,"Thread % : could not create thread",thID);
+            return 1;
+        }
+         
+        
+        
+    }
+     
+    if (new_socket<0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+     
+    return 0;
+}
+ 
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+     
+    char *message;
+     
+    //Send some messages to the client
+    message = "Greetings! I am your connection handler\n";
+    write(sock , message , strlen(message));
+     
+    message = "Its my duty to communicate with you";
+    write(sock , message , strlen(message));
+     
+    //Free the socket pointer
+    free(socket_desc);
+     
+    return 0;
+}
+
+
+
 int main(int argc, char **argv)
 {
     /* initialize the logging system */
@@ -511,6 +598,10 @@ int main(int argc, char **argv)
     pthread_t doThread;
     pthread_create (&doThread, NULL, deviceMonitoring, NULL);
 	
+	// Monitoring Ethernet Device
+	
+	pthread_t doThread;
+    pthread_create (&doThread, NULL, deviceEthernetMonitor, NULL);
     // Connect to devices
     openMultipleDevice();
     /* main loop */
