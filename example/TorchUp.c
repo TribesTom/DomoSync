c9477dba8df6285f687ed10e4e2219f8f0aaf08d
TribesTom <tribes.tom@gmail.com>
2014-09-05 15:54:37 +0200

    Ok

---
+++ example/TorchUp.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <mqueue.h>
+#include <unistd.h>
+#include "../src/Domo.h"
+#include "../src/mappin/mega2560.h"
+
+	
+int main(int argc, char **argv)
+{
+if(argc != 3 ) { 
+	printf("Erreur d'argument, utilisation : ./Switch DomoSy-... numPinDigital \n");
+	return -1 ;
+	}
+
+
+setShowLogs(1);
+
+DomoDevice dev = attachDomoDevice (argv[1]);
+
+int pin=pinMap[atoi(argv[2])];
+if (dev.fd != -1 )
+{
+//  setPinModeAsOutput (&dev, pin); // for debug
+
+setPinTorchUp(&dev, pin);
+}
+detachDomoDevice (&dev);
+
+    return 0;
+}
