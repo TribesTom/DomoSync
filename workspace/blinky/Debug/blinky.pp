5e8e5cef94db2fa2dc2e5fba3cc6db6d6b2a77a6
TribesTom <tribes.tom@gmail.com>
2015-04-28 12:38:06 +0200

    place

--- workspace/blinky/Debug/blinky.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-blinky.obj: ../blinky.c
-blinky.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.9/include/stdint.h
-blinky.obj: C:/ti/TivaWareC/inc/tm4c1294ncpdt.h
-
-../blinky.c: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.9/include/stdint.h: 
-C:/ti/TivaWareC/inc/tm4c1294ncpdt.h: 
--- workspace/senshub_iot/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/ti/TivaWareC/examples/boards/ek-tm4c1294xl-boostxl-senshub/senshub_iot/ccs/../../../../../driverlib/ccs/Debug/driverlib.lib" -l"C:/ti/TivaWareC/examples/boards/ek-tm4c1294xl-boostxl-senshub/senshub_iot/ccs/../../../../../sensorlib/ccs/Debug/sensorlib.lib"
-
